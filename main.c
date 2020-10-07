245ab53bad2d3acf469a17498f3850643d5b0ea7
viniciusddrft <viniciusddrft5@gmail.com>
2020-10-07 17:00:04 -0300

    versão usável

--- main.c
+++ main.c
@@ -7,22 +7,65 @@
 #include <pthread.h>
 #include <omp.h>
 // bibliotecas para sockets
-#include <netdb.h>//só precisa dessa
+//#include <netdb.h>  as bibliotecas abaixo substitui essa melhor
+#include <sys/types.h>    // AF_INET, SOCK_STREAM
+#include <sys/socket.h>   // socket(), connect()
+#include <netinet/in.h>   // struct sockaddr_in
+#include <arpa/inet.h>    // htons(), inet_addr()
+#include <unistd.h>       // close()
+
 
 #define LEN 4096
 
 
+void manual();
 
-void server(){
+void server(int porta);
 
-    
+void cliente(int porta , char ip[]);
+
+//---------------------------------------------------------------------------------------------------------------------------------------------
+int main(int argc, char *argv[]){
+    //ponteiro para struct que armazena data e hora  
+    struct tm *data_hora_atual;    
+  
+    //variável do tipo time_t para armazenar o tempo em segundos  
+    time_t segundos;
+  
+    //obtendo o tempo em segundos  
+    time(&segundos);   
+  
+    //para converter de segundos para o tempo local utilizamos a função localtime  
+    data_hora_atual = localtime(&segundos);  
+  
+    //para acessar os membros de uma struct usando o ponteiro utilizamos o operador ->
+
+    printf("\nRelógio -> %d:%d:%d\n",data_hora_atual->tm_hour, data_hora_atual->tm_min, data_hora_atual->tm_sec);//hora minuto e segundo
+
+    if (argc==4 && !strcmp("--server",argv[1]) && !strcmp("-p",argv[2])){
+        int porta = atoi(argv[3]);
+        server(porta);
+    }
+    else if (argc==5 && !strcmp("--client",argv[1]) && !strcmp("-p",argv[2])){
+        int porta = atoi(argv[3]);
+        cliente(porta , argv[4]);
+    }
+    else{
+        manual();
+    }
+    return 0;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------
+void manual(){
+    printf("Exemplo de como usar como servidor ->  ./main --server -p <PORT>\n");
+    printf("Exemplo de como usar como cliente -> ./main --client -p <PORT> <IP>\n");
+}
+
+void server(int porta){
     //struct usadas na parte do servidor
     struct sockaddr_in localhost;
     struct sockaddr_in cliente;
     struct tm *data_hora_atual;//ponteiro para o struct
-
-    //constantes usadas na parte do servidor
-    const int PORTA = 3000;
 
     time_t segundos; // variável usada para o tempo
 
@@ -46,7 +89,7 @@ void server(){
 	}
 
     localhost.sin_family = AF_INET;// definindo protocolo
-	localhost.sin_port = htons(PORTA);
+	localhost.sin_port = htons(porta);
 	memset(localhost.sin_zero, 0x0, 8);
 
     if (bind(meu_socket,(struct sockaddr*)&localhost,sizeof(localhost)) == -1){// atribui o socket a uma porta
@@ -69,9 +112,6 @@ void server(){
             if((tamanho_da_resposta = recv(cliente_socket,mensagem,LEN,0)) > 0){
                 mensagem[tamanho_da_resposta-1] = '\0';
 
-                if (!strcmp(mensagem,"exit")){//codigo para encerar o servidor quando o cliente encerar o socket dele
-                    close(cliente_socket);//refatorar
-                }
 
                 time(&segundos);
                 data_hora_atual = localtime(&segundos);
@@ -82,16 +122,11 @@ void server(){
     }
     //------------------------------------------------------------------------------------------------
     void *enviar_msg(){
-        while(true){
-            time(&segundos);
-            data_hora_atual = localtime(&segundos);
-            printf("%d:%d:%d| Você -> ",data_hora_atual->tm_hour, data_hora_atual->tm_min, data_hora_atual->tm_sec);
+        while(true){;
             // scanf pega o texto ate achar uma palavra em branco apenas por isso ele só pega uma palavra
             fgets(mensagem,LEN,stdin);//diferente do scanf eu consigo com essa função mandar mensagens com mais de uma palavra;
             (send(cliente_socket,mensagem, strlen(mensagem), 0));
-            if (!strcmp(mensagem,"exit")){//refatorar  
-                close(cliente_socket);
-            }
+
         }
     }
     pthread_t thread_server_receber_msg, thread_server_enviar_msg;
@@ -103,17 +138,14 @@ void server(){
         pthread_join(thread_server_receber_msg,NULL);
         pthread_join(thread_server_enviar_msg,NULL);
 	}
-    close(meu_socket);
-	printf("Servidor Encerrado\n");
+
+
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------
-void cliente(){
+void cliente(int porta , char ip[]){
     //struct usadas na parte do cliente
     struct sockaddr_in servidor;
     struct tm *data_hora_atual; //ponteiro para o struct
-
-    //constantes usadas na parte do cliente
-    const int PORTA = 3000;
 
     time_t segundos; // variável usada para o tempo
 
@@ -140,8 +172,8 @@ void cliente(){
     scanf("%s", apelido);
 
 	servidor.sin_family = AF_INET;// definindo protocolo
-	servidor.sin_port = htons(PORTA); 
-	servidor.sin_addr.s_addr = inet_addr("127.0.0.1");//ip do servidor que desejamos nos conectar
+	servidor.sin_port = htons(porta); 
+	servidor.sin_addr.s_addr = inet_addr(ip);//ip do servidor que desejamos nos conectar
 	memset(servidor.sin_zero, 0x0, 8);
 
     if((connect(meu_socket,(struct sockaddr*)&servidor, tamanho)) == -1){
@@ -153,10 +185,6 @@ void cliente(){
         while(true){
             if((tamanho_da_mensagem = recv(meu_socket,mensagem,LEN,0)) > 0){
                 mensagem[tamanho_da_mensagem-1] = '\0';
-                if (!strcmp(mensagem,"exit")){//refatorar
-                    printf("Cliente Encerrado\n");
-                    close(meu_socket);
-                }    
 
                 time(&segundos);
                 data_hora_atual = localtime(&segundos);
@@ -168,17 +196,11 @@ void cliente(){
     //------------------------------------------------------------------------------------------------
     void *enviar_msg(){
         while(true){
-            time(&segundos);
-            data_hora_atual = localtime(&segundos);
             memset(mensagem, 0x0,LEN);
-            printf("%d:%d:%d| Você -> ",data_hora_atual->tm_hour, data_hora_atual->tm_min, data_hora_atual->tm_sec);
             //scanf("%s", &mensagem);
             fgets(mensagem,LEN,stdin);// codigo que eu usava antes e foi trocado pela linha de cima
             send(meu_socket,mensagem, strlen(mensagem), 0);
-            if (!strcmp(mensagem,"exit\n")){//refatorar tem 4 comparações iguais a essa
-                printf("Cliente Encerrado\n");
-                close(meu_socket);
-            }
+
         }
     }
     pthread_t thread_cliente_receber_msg, thread_cliente_enviar_msg;
@@ -188,41 +210,4 @@ void cliente(){
     pthread_create(&thread_cliente_enviar_msg,NULL, enviar_msg, NULL);
     pthread_join(thread_cliente_receber_msg,NULL);
     pthread_join(thread_cliente_enviar_msg,NULL);
-}
-//---------------------------------------------------------------------------------------------------------------------------------------------
-int main(int argc, char *argv[]){
-    int escolha;
-
-    //ponteiro para struct que armazena data e hora  
-    struct tm *data_hora_atual;    
-  
-    //variável do tipo time_t para armazenar o tempo em segundos  
-    time_t segundos;
-  
-    //obtendo o tempo em segundos  
-    time(&segundos);   
-  
-    //para converter de segundos para o tempo local utilizamos a função localtime  
-    data_hora_atual = localtime(&segundos);  
-  
-    //para acessar os membros de uma struct usando o ponteiro utilizamos o operador ->
-
-    printf("\nRelógio -> %d:%d:%d\n",data_hora_atual->tm_hour, data_hora_atual->tm_min, data_hora_atual->tm_sec);//hora minuto e segundo
-
-    printf("Olá seja bem-vindo ao meu software de comunicação por terminal\n");
-    printf("\n");
-    printf("você pode se conectar a um servidor ou ser o servidor para alguém se conectar a você\n");
-    printf("Digite 1 para ser o servidor\n");
-    printf("Digite 2 para se conectar a um servidor\n");
-    scanf("%d", &escolha);
-    if (escolha == 1){
-        server();
-    }
-    else if (escolha == 2){
-        cliente();
-    }
-    else{
-        printf("escolha invalida\n");
-    }
-    return 0;
 }
